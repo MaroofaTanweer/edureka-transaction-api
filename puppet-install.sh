67898e4ba0a1539e5217e4b5b30bb909f16a72cb
Maroofa Tanweer <70618235+MaroofaTanweer@users.noreply.github.com>
2021-07-28 22:13:49 -0400

    Update puppet-install.sh

--- puppet-install.sh
+++ puppet-install.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 wget https://apt.puppetlabs.com/puppet6-release-focal.deb
 echo Stranger@1989 | sudo -S dpkg -i puppet6-release-focal.deb
-echo Stranger@1989 | sudo -S apt update -y
-echo Stranger@1989 | sudo -S apt install puppet-agent -y
+echo Stranger@1989 | sudo -S yum update -y
+echo Stranger@1989 | sudo -S yum install puppet-agent -y
 echo Stranger@1989 | sudo -S sh -c 'echo "[main]" >  /etc/puppetlabs/puppet/puppet.conf'
-echo Stranger@1989 | sudo -S sh -c 'echo "certname = ubuntu-test" >>  /etc/puppetlabs/puppet/puppet.conf'
+echo Stranger@1989 | sudo -S sh -c 'echo "certname = sit-centos" >>  /etc/puppetlabs/puppet/puppet.conf'
 echo Stranger@1989 | sudo -S sh -c 'echo "server = dev-master" >>  /etc/puppetlabs/puppet/puppet.conf'
